2ea28765dc999a00b4d7954cb31233195e6ad2a5
Yannis Alouache <alouacehyannis1@gmail.com>
2025-02-08 13:25:07 +0100

    feat: add category domain model

--- backend/src/main/java/com/akazaki/api/domain/model/Category.java
+++ backend/src/main/java/com/akazaki/api/domain/model/Category.java
@@ -1,19 +1,33 @@
 package com.akazaki.api.domain.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.trim();
+    }
 }
