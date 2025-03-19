469fd0dcab08f827ece0a88bcd49cb67dc2e2866
Yannis Alouache <alouacehyannis1@gmail.com>
2025-03-19 20:33:00 +0100

    refactor(GlobalExceptionHandler): enhance error response with status

--- backend/src/main/java/com/akazaki/api/infrastructure/web/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/akazaki/api/infrastructure/web/exception/GlobalExceptionHandler.java
@@ -12,47 +12,47 @@ import org.springframework.web.multipart.MaxUploadSizeExceededException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    public record ErrorResponse(String message) {}
+    public record ErrorResponse(String reason, HttpStatus status) {}
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
-        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EmailAlreadyRegisteredException.class)
     public ResponseEntity<ErrorResponse> handleEmailAlreadyRegistered(EmailAlreadyRegisteredException ex) {
-        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<ErrorResponse> handleInvalidCredentials(InvalidCredentialsException ex) {
-        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED);
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(CategoryAlreadyExistException.class)
     public ResponseEntity<ErrorResponse> handleCategoryAlreadyExist(CategoryAlreadyExistException ex) {
-        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UnableToFetchCategoriesException.class)
     public ResponseEntity<ErrorResponse> handleUnableToFetchCategories(UnableToFetchCategoriesException ex) {
-        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ProductAlreadyExistException.class)
     public ResponseEntity<ErrorResponse> handleProductAlreadyExist(ProductAlreadyExistException ex) {
-        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
-        ErrorResponse error = new ErrorResponse("Uploaded files cannot exceed 10Mo");
+        ErrorResponse error = new ErrorResponse("Uploaded files cannot exceed 10Mo", HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
