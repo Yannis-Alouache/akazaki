69983cf28ee5255aacd249d426f06c868190d226
Yannis Alouache <alouacehyannis1@gmail.com>
2025-03-19 20:16:29 +0100

    fix(exception-handler): add an handler for file size

--- backend/src/main/java/com/akazaki/api/infrastructure/web/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/akazaki/api/infrastructure/web/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.akazaki.api.infrastructure.web.exception;
 
 import com.akazaki.api.domain.exceptions.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 
 @RestControllerAdvice
@@ -21,7 +23,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmailAlreadyRegisteredException.class)
     public ResponseEntity<ErrorResponse> handleEmailAlreadyRegistered(EmailAlreadyRegisteredException ex) {
         ErrorResponse error = new ErrorResponse(ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.OK);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
@@ -45,6 +47,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ProductAlreadyExistException.class)
     public ResponseEntity<ErrorResponse> handleProductAlreadyExist(ProductAlreadyExistException ex) {
         ErrorResponse error = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+        ErrorResponse error = new ErrorResponse("Uploaded files cannot exceed 10Mo");
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
