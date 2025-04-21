eda6344356ed5f522c6c6c5e4cd9e81c67655c32
Yannis Alouache <alouacehyannis1@gmail.com>
2025-04-21 21:27:58 +0200

    refactor: replace autowired fields with constructor injection

--- backend/src/main/java/com/akazaki/api/application/commands/AddToCart/AddToCartCommandHandler.java
+++ backend/src/main/java/com/akazaki/api/application/commands/AddToCart/AddToCartCommandHandler.java
@@ -1,6 +1,10 @@
 package com.akazaki.api.application.commands.AddToCart;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+
+import com.akazaki.api.domain.exceptions.UserNotFoundException;
+import com.akazaki.api.domain.model.User;
+import com.akazaki.api.domain.ports.out.UserRepository;
 import org.springframework.stereotype.Component;
 
 import com.akazaki.api.domain.exceptions.ProductNotFoundException;
@@ -17,21 +21,25 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class AddToCartCommandHandler {
-    @Autowired
-    CartRepository cartRepository;
-
-    @Autowired
-    ProductRepository productRepository;
-
-    public void handle(AddToCartCommand command) {
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+    public Cart handle(AddToCartCommand command) {
         int quantity = 1;
 
+        User user = userRepository.findById(command.userId()).orElseThrow(
+            () -> new UserNotFoundException(command.userId())
+        );
+
         Cart cart = cartRepository.findByUserId(command.userId()).orElse(
-            Cart.builder().build()
+            Cart.builder()
+                .user(user)
+                .cartItems(new ArrayList<>())
+                .build()
         );
 
         Product product = productRepository.findById(command.productId()).orElseThrow(
-            () -> new ProductNotFoundException()
+            ProductNotFoundException::new
         );
 
         if (product.getStock() < quantity) throw new ProductOutOfStockException();
@@ -43,6 +51,6 @@ public class AddToCartCommandHandler {
         
         cart.addItem(cartItem);
 
-        cartRepository.save(cart);
+        return cartRepository.save(cart);
     }
 }
