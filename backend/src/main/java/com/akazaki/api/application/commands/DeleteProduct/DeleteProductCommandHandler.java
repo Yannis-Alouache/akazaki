8a59784314757ea5c114372e0d7d35a01bf17ebd
ElChaher <elchaher.af@icloud.com>
2025-04-26 17:11:28 +0200

    feat: implement test E2E delete product

--- backend/src/main/java/com/akazaki/api/application/commands/DeleteProduct/DeleteProductCommandHandler.java
+++ backend/src/main/java/com/akazaki/api/application/commands/DeleteProduct/DeleteProductCommandHandler.java
@@ -1,20 +1,50 @@
 package com.akazaki.api.application.commands.DeleteProduct;
 
 import com.akazaki.api.domain.exceptions.ProductNotFoundException;
+import com.akazaki.api.domain.model.Product;
 import com.akazaki.api.domain.ports.in.commands.DeleteProductCommand;
 import com.akazaki.api.domain.ports.out.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class DeleteProductCommandHandler {
+    private static final Logger logger = LoggerFactory.getLogger(DeleteProductCommandHandler.class);
     private final ProductRepository productRepository;
 
     public void handle(DeleteProductCommand command) {
-        if (productRepository.findById(command.productId()).isEmpty()) {
-            throw new ProductNotFoundException();
+
+        Optional<Product> optionalProduct = productRepository.findById(command.productId());
+        if (optionalProduct.isEmpty()) {
+            logger.warn("Produit avec l'ID {} introuvable.", command.productId());
+            return;
         }
+
+        Product product = optionalProduct.get();
+
+        String imageUrl = product.getImageUrl();
+        if (imageUrl != null) {
+            File imageFile = new File("uploads/" + imageUrl);
+            if (imageFile.exists()) {
+                boolean deleted = imageFile.delete();
+                if (deleted) {
+                    logger.info("Image {} supprimée avec succès.", imageUrl);
+                } else {
+                    logger.warn("Échec de la suppression de l'image {}.", imageUrl);
+                }
+            } else {
+                logger.warn("Image {} introuvable dans le dossier d'upload.", imageUrl);
+            }
+        }
+
         productRepository.deleteById(command.productId());
+        logger.info("Produit avec l'ID {} supprimé avec succès.", command.productId());
     }
 }
+
--- backend/src/main/java/com/akazaki/api/infrastructure/persistence/Product/JpaProductRepository.java
+++ backend/src/main/java/com/akazaki/api/infrastructure/persistence/Product/JpaProductRepository.java
@@ -2,6 +2,7 @@ package com.akazaki.api.infrastructure.persistence.Product;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
