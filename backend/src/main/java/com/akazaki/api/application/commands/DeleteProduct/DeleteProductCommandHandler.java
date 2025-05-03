746824b81b1b6c836e422300d7d8ae47e475dc59
Yannis Alouache <alouacehyannis1@gmail.com>
2025-05-03 17:47:44 +0200

    refactor: improve code and delete image before product

--- backend/src/main/java/com/akazaki/api/application/commands/DeleteProduct/DeleteProductCommandHandler.java
+++ backend/src/main/java/com/akazaki/api/application/commands/DeleteProduct/DeleteProductCommandHandler.java
@@ -4,6 +4,7 @@ import com.akazaki.api.domain.exceptions.ProductNotFoundException;
 import com.akazaki.api.domain.model.Product;
 import com.akazaki.api.domain.ports.in.commands.DeleteProductCommand;
 import com.akazaki.api.domain.ports.out.ProductRepository;
+import com.akazaki.api.infrastructure.exceptions.UnableToDeleteFileException;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,33 +16,25 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 public class DeleteProductCommandHandler {
-    private static final Logger logger = LoggerFactory.getLogger(DeleteProductCommandHandler.class);
     private final ProductRepository productRepository;
 
     @Value("${upload.dir}")
     private String uploadDir;
 
     public void handle(DeleteProductCommand command) {
-        Optional<Product> optionalProduct = productRepository.findById(command.productId());
-        if (optionalProduct.isEmpty()) {
-            throw new ProductNotFoundException();
-        }
+        Product product = productRepository.findById(command.productId()).orElseThrow(
+            ProductNotFoundException::new
+        );
 
-        Product product = optionalProduct.get();
         String imageUrl = product.getImageUrl();
-
-        try {
-            productRepository.deleteById(command.productId());
-            logger.info("Produit avec l'ID {} supprimé de la base de données.", command.productId());
-        } catch (Exception e) {
-            throw new IllegalStateException("Erreur lors de la suppression du produit", e);
-        }
 
         if (imageUrl != null) {
             File imageFile = new File(uploadDir, imageUrl.replace("/uploads/", ""));
             if (imageFile.exists() && !imageFile.delete()) {
-                throw new IllegalStateException("Échec de la suppression de l'image " + imageUrl);
+                throw new UnableToDeleteFileException();
             }
         }
+
+        productRepository.deleteById(command.productId());
     }
 }
