4ecac33cc6f123b53cb2fa0f51892528c0d0042c
Yannis Alouache <57330326+Yannis-Alouache@users.noreply.github.com>
2025-05-10 20:37:09 +0200

    feat: addToCart E2E test (#48)

--- backend/src/test/java/com/akazaki/api/config/JwtFactory.java
+++ backend/src/test/java/com/akazaki/api/config/JwtFactory.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.akazaki.api.domain.model.User;
 import com.akazaki.api.infrastructure.security.JwtService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtFactory {
-
     @Autowired
     private JwtService jwtService;
 
