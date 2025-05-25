a183a9e6833626d3523690734d562dcaa0dc8c5b
Yannis-Alouache <alouacehyannis1@gmail.com>
2025-05-25 15:52:44 +0200

    refactor: update Address model to use constructor and static factory method for instantiation

--- backend/src/main/java/com/akazaki/api/domain/model/Address.java
+++ backend/src/main/java/com/akazaki/api/domain/model/Address.java
@@ -1,40 +1,63 @@
 package com.akazaki.api.domain.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Address {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String lastName;
-
-    @Column(nullable = false)
     private String firstName;
-
-    @Column(nullable = false)
     private String streetNumber;
-
-    @Column(nullable = false)
     private String street;
-
-    @Column(nullable = true)
     private String addressComplement;
-
-    @Column(nullable = false)
     private String postCode;
-
-    @Column(nullable = false)
     private String city;
-
-    @Column(nullable = false)
     private String country;
+
+    private Address(
+        Long id,
+        String lastName,
+        String firstName,
+        String streetNumber,
+        String street,
+        String addressComplement,
+        String postCode,
+        String city,
+        String country
+    ) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.streetNumber = streetNumber;
+        this.street = street;
+        this.addressComplement = addressComplement;
+        this.postCode = postCode;
+        this.city = city;
+        this.country = country;
+    }
+
+    public static Address create(
+        String lastName,
+        String firstName,
+        String streetNumber,
+        String street,
+        String addressComplement,
+        String postCode,
+        String city,
+        String country
+    ) {
+        return new Address(null, lastName, firstName, streetNumber, street, addressComplement, postCode, city, country);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                    "id=" + id +
+                    ", lastName='" + lastName + '\'' +
+                    ", firstName='" + firstName + '\'' +
+                    ", streetNumber='" + streetNumber + '\'' +
+                    ", street='" + street + '\'' +
+                    ", addressComplement='" + addressComplement + '\'' +
+                    ", postCode='" + postCode + '\'' +
+                    ", city='" + city + '\'' +
+                    ", country='" + country + '\'' +
+                '}';
+    }
 }
