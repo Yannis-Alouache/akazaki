d7132042ec389375e0f214470278e0becc04db15
Yannis-Alouache <alouacehyannis1@gmail.com>
2025-05-29 16:23:39 +0200

    feat: introduce OrderRepository interface and its Jpa implementation for order persistence

---
+++ backend/src/main/java/com/akazaki/api/infrastructure/persistence/Order/JpaOrderRepository.java
@@ -0,0 +1,7 @@
+package com.akazaki.api.infrastructure.persistence.Order;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JpaOrderRepository extends CrudRepository<OrderEntity, Long> {
+    
+}
