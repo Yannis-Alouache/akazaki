ad4a6bbf69ef1e23c54754e97b1e41e5701a0572
Yannis-Alouache <alouacehyannis1@gmail.com>
2025-05-30 13:32:23 +0200

    feat: integrate user retrieval in CreateOrderCommandHandler for order creation

--- backend/src/main/java/com/akazaki/api/application/commands/CreateOrder/CreateOrderCommandHandler.java
+++ backend/src/main/java/com/akazaki/api/application/commands/CreateOrder/CreateOrderCommandHandler.java
@@ -9,30 +9,40 @@ import com.akazaki.api.domain.model.Cart;
 import com.akazaki.api.domain.model.Order;
 import com.akazaki.api.domain.model.OrderItem;
 import com.akazaki.api.domain.model.OrderStatus;
+import com.akazaki.api.domain.model.User;
 import com.akazaki.api.domain.ports.in.commands.CreateOrderCommand;
 import com.akazaki.api.domain.ports.out.CartRepository;
 import com.akazaki.api.domain.ports.out.OrderRepository;
+import com.akazaki.api.domain.ports.out.UserRepository;
 
 @Component
 public class CreateOrderCommandHandler {
     private final CartRepository cartRepository;
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
 
     public CreateOrderCommandHandler(
         CartRepository cartRepository,
-        OrderRepository orderRepository
+        OrderRepository orderRepository,
+        UserRepository userRepository
     ) {
         this.cartRepository = cartRepository;
         this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
     }
 
     public Order handle(CreateOrderCommand command) {
+        User user = userRepository.findById(command.userId()).orElseThrow(
+            () -> new UnableToCreateOrderException()
+        );
+
         Cart cart = cartRepository.findByUserId(command.userId()).orElseThrow(
             () -> new UnableToCreateOrderException()
         );
         validateCart(cart);
 
         Order order = Order.draft(
+            user,
             LocalDateTime.now(),
             OrderStatus.PENDING,
             OrderItem.fromCartItems(cart.getCartItems()),
