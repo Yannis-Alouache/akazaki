d9e2d30068876bbfc68b7e2d331b580f69c7d98b
Yannis-Alouache <alouacehyannis1@gmail.com>
2025-05-30 15:10:53 +0200

    feat: configure Stripe API key dynamically using application properties in StripePaymentIntentsGateway

--- backend/src/main/java/com/akazaki/api/infrastructure/stripe/StripePaymentIntentsGateway.java
+++ backend/src/main/java/com/akazaki/api/infrastructure/stripe/StripePaymentIntentsGateway.java
@@ -1,8 +1,11 @@
 package com.akazaki.api.infrastructure.stripe;
 
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.akazaki.api.domain.model.Order;
 import com.akazaki.api.domain.ports.out.PaymentGateway;
@@ -11,8 +14,13 @@ import com.akazaki.api.domain.ports.out.PaymentGateway;
 @Component
 public class StripePaymentIntentsGateway implements PaymentGateway {
 
+    @Value("${stripe.secret.key}")
+    private String stripeSecretKey;
+
     @Override
     public String createPaymentIntent(Order order) {
+        Stripe.apiKey = stripeSecretKey;
+
         try {
             PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
                 .setAmount(calculateTotalInCents(order))
