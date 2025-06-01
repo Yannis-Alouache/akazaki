8caeb177c43e7f142b6a8df05f544d11bd9c3095
Yannis-Alouache <alouacehyannis1@gmail.com>
2025-06-01 16:32:41 +0200

    chore: add TODO comments for future enhancements related to stock management and custom exceptions in BackendApplication

--- backend/src/main/java/com/akazaki/api/BackendApplication.java
+++ backend/src/main/java/com/akazaki/api/BackendApplication.java
@@ -3,8 +3,15 @@ package com.akazaki.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://docs.stripe.com/api/payment_intents
+// https://docs.stripe.com/payments/payment-intents
+// https://docs.stripe.com/payments/accept-a-payment?ui=elements
+
+// TODO: Add decrease stock when order is created
+// TODO: Add custom exception for order use case
 @SpringBootApplication
 public class BackendApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
