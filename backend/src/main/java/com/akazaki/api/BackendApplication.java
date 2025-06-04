73ebe861c291bf24c4313157cc77801550666c85
Yannis-Alouache <alouacehyannis1@gmail.com>
2025-06-04 22:10:36 +0200

    chore: update TODO comment in BackendApplication to check stock availability during payment processing

--- backend/src/main/java/com/akazaki/api/BackendApplication.java
+++ backend/src/main/java/com/akazaki/api/BackendApplication.java
@@ -7,12 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // https://docs.stripe.com/payments/payment-intents
 // https://docs.stripe.com/payments/accept-a-payment?ui=elements
 
-// TODO: Add decrease stock when order is created
+// TODO: check for stock when user try to pay for an order
 // TODO: Add custom exception for order use case
 // TODO: Fix conflict github
 @SpringBootApplication
 public class BackendApplication {
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
