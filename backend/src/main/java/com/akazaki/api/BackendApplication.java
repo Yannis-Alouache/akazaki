d24e6f0c2c7633d65fe69e174369b23a78447ee3
Yannis Alouache <alouacehyannis1@gmail.com>
2025-06-16 10:22:34 +0200

    chore: remove outdated comments and TODOs from BackendApplication.java

--- backend/src/main/java/com/akazaki/api/BackendApplication.java
+++ backend/src/main/java/com/akazaki/api/BackendApplication.java
@@ -3,12 +3,6 @@ package com.akazaki.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// https://docs.stripe.com/api/payment_intents
-// https://docs.stripe.com/payments/payment-intents
-// https://docs.stripe.com/payments/accept-a-payment?ui=elements
-
-// TODO: Fix conflict github
-// TODO: Add webhook for stripe payment intent TEST
 @SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
