0fd1754da47dbdecd80db9a3e092709f94943223
Azixy Dev <azizchegrani97@gmail.com>
2025-07-23 18:41:50 +0200

    update url

--- backend/src/main/java/com/akazaki/api/infrastructure/config/CorsConfig.java
+++ backend/src/main/java/com/akazaki/api/infrastructure/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")  // your frontend URL
+                        .allowedOrigins("https://akazaki.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
